d62f2777b726c10b8725d7433829a13af7510026
ivan ois <ivoxelkapo@gmail.com>
2021-12-12 18:58:28 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+let bugati = 1;    
+let yate = 2;
+let casa = 3;
+let dinero = 4;
+
+let sorpresa = prompt ("eleji una opcion viene con sorpresa (1,2,3,4)");
+
+if (sorpresa == bugati){
+    alert ("ganaste un bugati");
+}
+else if (sorpresa == yate){
+    alert ( "ganaste un yate");
+}
+else if ( sorpresa == casa){
+    alert ( "ganaste una casa");
+}
+else if (sorpresa == dinero ){
+    alert ("ganaste dinero");
+}
+else {
+    alert("no ganaste nada volve a elejir un numero correcto");
+}
